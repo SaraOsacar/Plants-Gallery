9305b90388c6ef35d3d3374c5ef470260266c83a
SaraOsacar <saraosacar@gmail.com>
2023-09-22 13:05:08 +0200

    change grayscale, add button,, scroll & plant 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 'use strict';
 
+let scrollContainer = document.querySelector('.gallery');
+let backButton = document.getElementById('backButton'); 
+let nextButton = document.getElementById('nextButton'); 
+
+
+scrollContainer.addEventListener('wheel', (ev) => {
+  ev.preventDefault();
+  scrollContainer.scrollLeft += ev.deltaY;
+});
+
+nextButton.addEventListener('click', () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+});
+
+backButton.addEventListener('click', () => {
+  scrollContainer.style.scrollBehavior = 'smooth';
+  scrollContainer.scrollLeft -= 900;
+});
+
+
+
+
+
+
